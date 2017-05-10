ac8247233224b9431e81db64f398d698d5e3b602
Andre <drinkins85@mail.ru>
2017-05-10 11:52:22 +0300

    ws2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import Index from './components/Index'
-//
+//--
 const store = configureStore();
 
 ReactDOM.render(<Provider store={store}><Index/></Provider>, document.getElementById("root"));
